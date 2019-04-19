5a875d633c731030a05537256bc524e736b597eb
Chris Symon <chris.symon97@gmail.com>
2019-04-19 14:51:39 +1200

    meteor init

--- client/screens/Users/UserSignInPending/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import UsersNavBar from '../components/UsersNavBar';
-
-export default () => (
-  <UsersNavBar label="Signing In" screen="user/signingin" />
-);
--- doc/CHANGELOG.md
+++
@@ -1,4 +0,0 @@
-# Notable changes to feathers-starter-react-redux-login-roles
-
-## 0.0.2
-- Added working starter code. Scripts set up for dev-mode.
