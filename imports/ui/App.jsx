3166a04ea4294604511ef1af7bdcc4a8f9bec5b7
Chris Symon <chris.symon97@gmail.com>
2019-04-23 18:36:04 +1200

    added css

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -6,9 +6,11 @@ class App extends Component {
 
   render() {
     return (
-      <div>
-        <h1>Welcome to Meteor!</h1>
-        <AccountsUIWrapper />
+      <div className="container">
+        <header>
+          <h1>JuryRoom</h1>
+          <AccountsUIWrapper />
+        </header>
       </div>
     );
   }
