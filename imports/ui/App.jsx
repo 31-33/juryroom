4f43c4dc3a965e01d059016cd4925f4dd5724b17
Chris Symon <chris.symon97@gmail.com>
2019-04-30 23:09:58 +1200

    added sample comments

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,8 +1,68 @@
 import React, { Component } from 'react';
 import AccountsUIWrapper from '/imports/ui/AccountsUIWrapper';
 import '/imports/startup/accounts-config';
+import DiscussionThread from '/imports/ui/DiscussionThread';
+import { Icon } from 'semantic-ui-react';
+
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+  }
+
+  comments = [
+        {
+          username: 'bob',
+          time: '(timestamp)',
+          text: 'hello world!',
+          voted: false
+        },
+        {
+          username: 'ann',
+          time: '(timestamp)',
+          text: 'hello world!',
+          voted: false,
+          children: [
+            {
+              username: 'bob',
+              time: '(timestamp)',
+              text: 'sub-comment1',
+              voted: false
+            },
+            {
+              username: 'ann',
+              time: '(timestamp)',
+              text: 'sub-comment2',
+              voted: false
+            },
+            {
+              username: 'foo',
+              time: '(timestamp)',
+              text: 'sub-comment3',
+              voted: false
+            },
+            {
+              username: 'bob',
+              time: '(timestamp)',
+              text: 'sub-comment4',
+              voted: false
+            },
+          ]
+        },
+        {
+          username: 'foo',
+          time: '(timestamp)',
+          text: 'bar',
+          voted: false
+        },
+        {
+          username: 'bob',
+          time: '(timestamp)',
+          text: 'test',
+          voted: false
+        },
+      ];
 
   render() {
     return (
@@ -11,6 +71,7 @@ class App extends Component {
           <h1>JuryRoom</h1>
           <AccountsUIWrapper />
         </header>
+        <DiscussionThread comments={this.comments} />
       </div>
     );
   }
