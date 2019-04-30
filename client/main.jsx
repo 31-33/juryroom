a05c94e4d3c1028c1c749da775f31017ab383937
Chris Symon <chris.symon97@gmail.com>
2019-04-30 23:07:22 +1200

    added semantic-ui-react

--- client/main.jsx
+++ client/main.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
+import 'semantic-ui-css/semantic.min.css';
 import App from '/imports/ui/App'
+
 
 Meteor.startup(() => {
   render(<App />, document.getElementById('react-target'));
