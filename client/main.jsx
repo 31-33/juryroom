2e618e3fd9082a0a34bf65f19f55f33bf01b8f72
Chris Symon <chris.symon97@gmail.com>
2019-05-01 23:53:52 +1200

    updated to use semantic-ui-meteor

--- client/main.jsx
+++ client/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
-import 'semantic-ui-css/semantic.min.css';
+// import 'semantic-ui-css';
 import App from '/imports/ui/App'
 
 
