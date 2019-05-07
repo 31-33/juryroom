cfb207c8513ba0891b4c3d572d4834b889433baf
Chris Symon <chris.symon97@gmail.com>
2019-05-07 16:36:02 +1200

    added floating component for voting

--- imports/ui/DiscussionThread.jsx
+++ imports/ui/DiscussionThread.jsx
@@ -1,22 +1,40 @@
-import React, { Component } from 'react';
-import { Comment } from 'semantic-ui-react';
+import React, { Component, createRef } from 'react';
+import { Comment, Container, Rail, Ref, Segment, Sticky } from 'semantic-ui-react';
 import CommentView from '/imports/ui/CommentView';
+import StarredCommentView from '/imports/ui/StarredCommentView';
 
 class DiscussionThread extends Component{
+  contextRef = createRef();
 
   renderComments(){
     return this.props.comments.map(comment =>  {
       return (
-        <CommentView data={comment} />
+        <CommentView key={comment.id} data={comment} />
       )
     });
   }
 
   render(){
     return (
-      <Comment.Group threaded='true'>
-        {this.renderComments()}
-      </Comment.Group>
+      <div>
+        <Ref innerRef={this.contextRef}>
+          <Segment>
+            <Comment.Group threaded={true}>
+              {this.renderComments()}
+            </Comment.Group>
+            <Rail position='left'>
+              <Sticky context={this.contextRef} offset={80}>
+                <StarredCommentView />
+              </Sticky>
+            </Rail>
+            <Rail position='right'>
+              <Sticky context={this.contextRef} offset={80}>
+                <Segment>Discussion history / navigation bar here</Segment>
+              </Sticky>
+            </Rail>
+          </Segment>
+        </Ref>
+      </div>
     );
   }
 }
---
+++ imports/ui/StarredCommentView.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Segment } from 'semantic-ui-react';
+
+class StarredCommentView extends Component {
+
+
+  render(){
+
+    return (
+      <Segment>Starred Comments!</Segment>
+    );
+  }
+}
+
+export default StarredCommentView;
