c49505776545c780258a6715a16bbea83f15dfc7
Chris Symon <chris.symon97@gmail.com>
2019-06-22 00:52:24 +1200

    Add 'Administrator' tag to admin user profile page

--- imports/ui/User/ModifyUserPermissions.jsx
+++ imports/ui/User/ModifyUserPermissions.jsx
@@ -13,42 +13,44 @@ class ModifyUserPermissions extends Component {
 
   render() {
     const { user } = this.props;
-    return user._id !== Meteor.userId()
-    && Roles.userIsInRole(Meteor.userId(), 'admin')
-    && !Roles.userIsInRole(user._id, 'admin')
-    && (
-      <Container>
-        <Header content="Permissions" attached="top" />
-        <Segment attached="bottom">
-          <List relaxed>
-            <List.Item>
-              <Checkbox
-                toggle
-                label="Create Scenarios"
-                checked={Roles.userIsInRole(user._id, 'create-scenario')}
-                onClick={(event, button) => Meteor.call('roles.setPermission_createScenario', user._id, button.checked)}
-              />
-            </List.Item>
-            <List.Item>
-              <Checkbox
-                toggle
-                label="Create Scenario Sets"
-                checked={Roles.userIsInRole(user._id, 'create-scenario-set')}
-                onClick={(event, button) => Meteor.call('roles.setPermission_createScenarioSet', user._id, button.checked)}
-              />
-            </List.Item>
-            <List.Item>
-              <Checkbox
-                toggle
-                label="Create Groups"
-                checked={Roles.userIsInRole(user._id, 'create-group')}
-                onClick={(event, button) => Meteor.call('roles.setPermission_createGroup', user._id, button.checked)}
-              />
-            </List.Item>
-          </List>
-        </Segment>
-      </Container>
-    );
+    return Roles.userIsInRole(user._id, 'admin')
+      ? <Segment inverted secondary color="green" content="Administrator" />
+      : user._id !== Meteor.userId()
+      && Roles.userIsInRole(Meteor.userId(), 'admin')
+      && !Roles.userIsInRole(user._id, 'admin')
+      && (
+        <Container>
+          <Header content="Permissions" attached="top" />
+          <Segment attached="bottom">
+            <List relaxed>
+              <List.Item>
+                <Checkbox
+                  toggle
+                  label="Create Scenarios"
+                  checked={Roles.userIsInRole(user._id, 'create-scenario')}
+                  onClick={(event, button) => Meteor.call('roles.setPermission_createScenario', user._id, button.checked)}
+                />
+              </List.Item>
+              <List.Item>
+                <Checkbox
+                  toggle
+                  label="Create Scenario Sets"
+                  checked={Roles.userIsInRole(user._id, 'create-scenario-set')}
+                  onClick={(event, button) => Meteor.call('roles.setPermission_createScenarioSet', user._id, button.checked)}
+                />
+              </List.Item>
+              <List.Item>
+                <Checkbox
+                  toggle
+                  label="Create Groups"
+                  checked={Roles.userIsInRole(user._id, 'create-group')}
+                  onClick={(event, button) => Meteor.call('roles.setPermission_createGroup', user._id, button.checked)}
+                />
+              </List.Item>
+            </List>
+          </Segment>
+        </Container>
+      );
   }
 }
 
