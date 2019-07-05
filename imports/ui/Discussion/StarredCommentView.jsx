c886854b00f8f40107c71f8b556164cb4badb1ab
Chris Symon <chris.symon97@gmail.com>
2019-07-05 22:16:27 +1200

    Improved starred comment box -shorten comment text -single feed for stars + active vote

--- imports/ui/Discussion/StarredCommentView.jsx
+++ imports/ui/Discussion/StarredCommentView.jsx
@@ -2,8 +2,10 @@ import React, { PureComponent } from 'react';
 import { Meteor } from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
 import {
-  Button, Comment, Header, Label, Segment, Container,
+  Button, Comment, Header, Segment, Container,
 } from 'semantic-ui-react';
+import ReactMarkdown from 'react-markdown';
+import Linkify from 'react-linkify';
 import Comments from '/imports/api/Comments';
 import Votes from '/imports/api/Votes';
 import PropTypes from 'prop-types';
@@ -13,6 +15,7 @@ import {
 import { Link } from 'react-router-dom';
 import Moment from 'react-moment';
 import { renderUserVotes } from '/imports/ui/Discussion/Vote';
+import Dotdotdot from 'react-dotdotdot';
 
 const scrollToElement = require('scroll-to-element');
 
@@ -29,156 +32,121 @@ class StarredCommentView extends PureComponent {
   }
 
   renderStarredComments(starredComments) {
-    const { comments, participants, discussion } = this.props;
+    const {
+      comments, participants, discussion, activeVote,
+    } = this.props;
 
     if (!starredComments.every(star => comments.some(comment => comment._id === star.commentId))) {
       return '';
     }
 
-    return (
-      <Container>
-        <Header
-          attached="top"
-          content="Starred Comments"
-          size="large"
-        />
-        <Segment attached="bottom">
-          <Comment.Group>
-            {
-              starredComments
-                .sort((c1, c2) => c2.users.length - c1.users.length)
-                .map((starredComment) => {
-                  const commentData = comments.find(
-                    comment => comment._id === starredComment.commentId,
-                  );
-                  const author = participants.find(user => user._id === commentData.authorId);
-                  const isStarred = starredComment.users.includes(Meteor.userId());
-                  return (
-                    <Comment key={starredComment.commentId}>
-                      <Comment.Content>
-                        <Comment.Author as={Link} to={`/user/${author._id}`}>
-                          {author.username}
-                        </Comment.Author>
-                        <Comment.Metadata>
-                          <Moment fromNow>{commentData.postedTime}</Moment>
-                          <Label
-                            basic
-                            as="a"
-                            onClick={() => scrollToElement(
-                              `#${starredComment.commentId}`,
-                              {
-                                align: 'top',
-                                offset: -120,
-                              },
-                            )}
-                            content="Show"
-                          />
-                        </Comment.Metadata>
-                        <Comment.Text>
-                          {commentData.text}
-                        </Comment.Text>
-                        <Comment.Actions>
-                          <Button
-                            attached="top"
-                            icon="star"
-                            color="yellow"
-                            content={isStarred ? 'Starred' : 'Star'}
-                            basic={!isStarred}
-                            label={{
-                              basic: true,
-                              color: 'yellow',
-                              pointing: 'left',
-                              content: starredComment.users.map(userId => participants.find(user => user._id === userId).username).join(', '),
-                            }}
-                            onClick={() => (isStarred
-                              ? Meteor.call('discussions.remove_star', discussion._id)
-                              : Meteor.call('discussions.star_comment', discussion._id, commentData._id))
-                            }
-                          />
-                          <Button
-                            disabled={!!discussion.activeVote
-                            || !starredComment.users.some(user => user === Meteor.userId())
-                            || discussion.votes.some(vote => vote.commentId === starredComment._id)
-                            || discussion.status !== 'active'}
-                            attached="bottom"
-                            icon="exclamation"
-                            color="green"
-                            content="Call Vote"
-                            onClick={() => Meteor.call('discussions.callVote', discussion._id, commentData._id, starredComment.users)}
-                          />
-                        </Comment.Actions>
-                      </Comment.Content>
-                    </Comment>
-                  );
-                })
-            }
-          </Comment.Group>
-        </Segment>
-      </Container>
-    );
-  }
+    return starredComments.map((starredComment) => {
+      const commentData = comments.find(
+        comment => comment._id === starredComment.commentId,
+      );
+      const author = participants.find(user => user._id === commentData.authorId);
+      const isStarred = starredComment.users.includes(Meteor.userId());
+      const isActiveVote = activeVote && activeVote.commentId === starredComment.commentId;
 
-  renderActiveVote(vote, commentData) {
-    const { participants } = this.props;
-    const author = participants.find(user => user._id === commentData.authorId);
-
-    return (
-      <Container>
-        <Header
-          attached="top"
-          content="Active Vote"
-          size="large"
-        />
-        <Segment attached="bottom" as={Comment.Group}>
-          <Comment key={commentData._id}>
-            <Comment.Content>
-              <Comment.Author as={Link} to={`/user/${author._id}`} content={author.username} />
-              <Comment.Metadata>
-                <Moment fromNow>{commentData.postedTime}</Moment>
-                <Label
-                  basic
-                  as="a"
-                  onClick={() => scrollToElement(
-                    `#${commentData._id}`,
-                    {
-                      align: 'top',
-                      offset: -120,
-                    },
-                  )}
-                  content="Show"
+      return (
+        <Comment key={starredComment.commentId}>
+          <Comment.Content>
+            <Comment.Author as={Link} to={`/user/${author._id}`}>
+              {author.username}
+            </Comment.Author>
+            <Comment.Metadata style={{ display: 'inline' }}>
+              <Moment fromNow>{commentData.postedTime}</Moment>
+              <Button
+                floated="right"
+                color="black"
+                size="mini"
+                onClick={() => scrollToElement(
+                  `#${starredComment.commentId}`,
+                  {
+                    align: 'top',
+                    offset: -120,
+                  },
+                )}
+                content="Scroll To"
+              />
+            </Comment.Metadata>
+            <Comment.Text>
+              <Linkify properties={{ target: '_blank' }}>
+                <Dotdotdot clamp={isActiveVote ? 10 : 5}>
+                  <ReactMarkdown source={commentData.text} />
+                </Dotdotdot>
+              </Linkify>
+            </Comment.Text>
+            <Comment.Actions>
+              {starredComment.users.length > 0 && (
+                <Button
+                  attached="top"
+                  icon="star"
+                  color="yellow"
+                  content={isStarred ? 'Starred' : 'Star'}
+                  basic={!isStarred}
+                  label={{
+                    basic: true,
+                    color: 'yellow',
+                    pointing: 'left',
+                    content: starredComment.users.map(userId => participants.find(user => user._id === userId).username).join(', '),
+                  }}
+                  onClick={() => (isStarred
+                    ? Meteor.call('discussions.remove_star', discussion._id)
+                    : Meteor.call('discussions.star_comment', discussion._id, commentData._id))
+                  }
                 />
-              </Comment.Metadata>
-              <Comment.Text content={commentData.text} />
-              <Comment.Actions>
-                {vote.userVotes.some(userVote => userVote.userId === Meteor.userId())
-                  ? renderUserVotes(vote, participants)
+              )}
+              {!isActiveVote && (
+                <Button
+                  disabled={!!discussion.activeVote
+                  || !starredComment.users.some(user => user === Meteor.userId())
+                  || discussion.votes.some(vote => vote.commentId === starredComment._id)
+                  || discussion.status !== 'active'}
+                  attached="bottom"
+                  icon="exclamation"
+                  color="green"
+                  content="Call Vote"
+                  onClick={() => Meteor.call('discussions.callVote', discussion._id, commentData._id, starredComment.users)}
+                />
+              )}
+              {isActiveVote && (
+                activeVote.userVotes.some(userVote => userVote.userId === Meteor.userId())
+                  ? renderUserVotes(activeVote, participants)
                   : (
                     <Button.Group fluid>
                       <Button
                         content="Disagree"
                         negative
-                        onClick={() => Meteor.call('votes.vote', vote._id, false)}
+                        onClick={() => Meteor.call('votes.vote', activeVote._id, false)}
                       />
                       <Button.Or />
                       <Button
                         content="Agree"
                         positive
-                        onClick={() => Meteor.call('votes.vote', vote._id, true)}
+                        onClick={() => Meteor.call('votes.vote', activeVote._id, true)}
                       />
                     </Button.Group>
                   )
-                }
-              </Comment.Actions>
-            </Comment.Content>
-          </Comment>
-        </Segment>
-      </Container>
-    );
+              )}
+            </Comment.Actions>
+          </Comment.Content>
+        </Comment>
+      );
+    });
   }
 
   render() {
-    const { comments, discussion, activeVote } = this.props;
+    const { discussion, activeVote } = this.props;
     const starredComments = [];
+
+    if (activeVote) {
+      starredComments.push({
+        commentId: activeVote.commentId,
+        users: [],
+      });
+    }
+
     discussion.userStars.forEach((star) => {
       const index = starredComments.findIndex(comment => comment.commentId === star.commentId);
       if (index < 0) {
@@ -191,13 +159,24 @@ class StarredCommentView extends PureComponent {
       }
     });
 
-    return (
+    starredComments.sort((starA, starB) => {
+      if (activeVote && activeVote.commentId === starA.commentId) return -1;
+      if (activeVote && activeVote.commentId === starB.commentId) return 1;
+      return starA.users.length - starB.users.length;
+    });
+
+    return starredComments.length > 0 && (
       <Container>
-        {activeVote && this.renderActiveVote(
-          activeVote,
-          comments.find(comment => comment._id === activeVote.commentId),
-        )}
-        {starredComments.length > 0 && this.renderStarredComments(starredComments)}
+        <Header
+          attached="top"
+          content="Starred Comments"
+          size="large"
+        />
+        <Segment attached="bottom">
+          <Comment.Group>
+            {this.renderStarredComments(starredComments)}
+          </Comment.Group>
+        </Segment>
       </Container>
     );
   }
