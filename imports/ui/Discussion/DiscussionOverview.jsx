e8d69f536cbe0248607255ce4a0c9c8ecfd9a22f
Chris Symon <chris.symon97@gmail.com>
2019-07-14 13:26:17 +1200

    Implemented discussion minimap/overview -coloured to show star/vote events -click to scroll

---
+++ imports/ui/Discussion/DiscussionOverview.jsx
@@ -0,0 +1,82 @@
+import React, { PureComponent } from 'react';
+import { Meteor } from 'meteor/meteor';
+import { withTracker } from 'meteor/react-meteor-data';
+import PropTypes from 'prop-types';
+import {
+  Segment, Container,
+} from 'semantic-ui-react';
+import Comments from '/imports/api/Comments';
+import Votes from '/imports/api/Votes';
+
+class DiscussionOverview extends PureComponent {
+  static propTypes = {
+    discussionId: PropTypes.string.isRequired,
+    handleClick: PropTypes.func.isRequired,
+  }
+
+  renderChildren = withTracker(({ discussionId, commentId }) => ({
+    children: Comments.find(
+      { parentId: commentId, discussionId },
+      {
+        fields: { _id: 1 },
+        sort: { postedTime: 1 },
+      },
+    ).fetch(),
+    comment: Comments.findOne(
+      { discussionId, _id: commentId },
+      { fields: { userStars: 1 } },
+    ),
+    vote: Votes.findOne({ discussionId, commentId }),
+  }))(({
+    discussionId, children, handleClick, comment, vote,
+  }) => {
+    const isStarred = comment
+      && comment.userStars
+      && comment.userStars.some(star => star.userId === Meteor.userId());
+
+    let color = 'black';
+    if (vote) {
+      color = 'green';
+    } else if (isStarred) {
+      color = 'yellow';
+    }
+
+    return (
+      <Container style={{ paddingLeft: '10px' }}>
+        {comment && (
+          <Container
+            style={{
+              height: '6px',
+              backgroundColor: color,
+              border: `${(vote || isStarred) ? '1px' : '2px'} solid #DEDEDE`,
+            }}
+            onClick={() => handleClick(comment._id)}
+          />
+        )}
+        {children.map(({ _id }) => (
+          <this.renderChildren
+            key={_id}
+            discussionId={discussionId}
+            commentId={_id}
+            handleClick={handleClick}
+          />
+        ))}
+      </Container>
+    );
+  });
+
+  render() {
+    const { discussionId, handleClick } = this.props;
+
+    return (
+      <Segment>
+        <this.renderChildren
+          discussionId={discussionId}
+          commentId=""
+          handleClick={handleClick}
+        />
+      </Segment>
+    );
+  }
+}
+export default DiscussionOverview;
