1073c5f6bf1f47600fffc77b042505fbb3c22d78
Chris Symon <chris.symon97@gmail.com>
2019-07-25 20:59:38 +1200

    Embed html in emails -comment + vote notifications

--- imports/ui/EmailTemplates/VoteNotification.jsx
+++ imports/ui/EmailTemplates/VoteNotification.jsx
@@ -31,54 +31,57 @@ class VoteNotification extends PureComponent {
       discussionId, author, caller, comment, scenario,
     } = this.props;
     return (
-      <Container>
-        {/* TODO: Embedd css in email */}
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css" />
-        <Segment attached="top">
-          <Header
-            size="huge"
-            content="JuryRoom"
-            icon="balance scale"
-          />
-        </Segment>
-        <Segment attached="bottom">
-          <p>
-            <em>{caller.username}</em>
-            {' '}
-            called a vote on the discussion
-            {' '}
-            <em>{`'${scenario.title}'`}</em>
-          </p>
-          <Segment raised>
-            <Comment.Group>
-              <Comment>
-                <Comment.Content>
-                  <Comment.Avatar src={author.avatar || `${process.env.ROOT_URL}/avatar_default.png`} style={{ height: '2.5em' }} />
-                  <Comment.Author content={author.username} />
-                  <Comment.Text>
-                    <Linkify properties={{ target: '_blank' }}>
-                      <ReactMarkdown
-                        source={comment.text}
-                        disallowedTypes={['image', 'imageReference']}
-                      />
-                    </Linkify>
-                  </Comment.Text>
-                </Comment.Content>
-              </Comment>
-            </Comment.Group>
+      <html lang="en">
+        <head>
+          <style>{Assets.getText('semantic.min.css')}</style>
+        </head>
+        <Container as="body">
+          <Segment attached="top">
+            <Header
+              size="huge"
+              content="JuryRoom"
+              icon="balance scale"
+            />
           </Segment>
-          Click
-          {' '}
-          <a href={`${process.env.ROOT_URL}discussion/${discussionId}`}>here</a>
-          {' '}
-          to view the discussion.
-          <Container>
-            <Linkify properties={{ target: '_blank' }}>
-              {`${process.env.ROOT_URL}discussion/${discussionId}`}
-            </Linkify>
-          </Container>
-        </Segment>
-      </Container>
+          <Segment attached="bottom">
+            <p>
+              <em>{caller.username}</em>
+              {' '}
+              called a vote on the discussion
+              {' '}
+              <em>{`'${scenario.title}'`}</em>
+            </p>
+            <Segment raised>
+              <Comment.Group>
+                <Comment>
+                  <Comment.Content>
+                    <Comment.Avatar src={author.avatar || `${process.env.ROOT_URL}/avatar_default.png`} style={{ height: '2.5em' }} />
+                    <Comment.Author content={author.username} />
+                    <Comment.Text>
+                      <Linkify properties={{ target: '_blank' }}>
+                        <ReactMarkdown
+                          source={comment.text}
+                          disallowedTypes={['image', 'imageReference']}
+                        />
+                      </Linkify>
+                    </Comment.Text>
+                  </Comment.Content>
+                </Comment>
+              </Comment.Group>
+            </Segment>
+            Click
+            {' '}
+            <a href={`${process.env.ROOT_URL}discussion/${discussionId}`}>here</a>
+            {' '}
+            to view the discussion.
+            <Container>
+              <Linkify properties={{ target: '_blank' }}>
+                {`${process.env.ROOT_URL}discussion/${discussionId}`}
+              </Linkify>
+            </Container>
+          </Segment>
+        </Container>
+      </html>
     );
   }
 }
