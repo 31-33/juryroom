dd2d9c05e57642fcdb07365cfceef910349c326c
Chris Symon <chris.symon97@gmail.com>
2019-08-27 20:49:33 +1200

    Publish all groups to admin

--- imports/api/Groups.js
+++ imports/api/Groups.js
@@ -1,22 +1,35 @@
 import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
+import { Roles } from 'meteor/alanning:roles';
 
 const Groups = new Mongo.Collection('groups');
 export default Groups;
 
 if (Meteor.isServer) {
   Meteor.publish('groups', function() {
-    return Groups.find(
-      { members: this.userId },
-      {
-        fields: {
-          members: 1,
-          scenarioSetId: 1,
-          discussions: 1,
-          createdAt: 1,
-          // TODO: add fields to track scoring
+    return Roles.userIsInRole(this.userId, 'admin')
+      ? Groups.find(
+        {},
+        {
+          fields: {
+            members: 1,
+            scenarioSetId: 1,
+            discussions: 1,
+            createdAt: 1,
+          },
         },
-      },
-    );
+      )
+      : Groups.find(
+        { members: this.userId },
+        {
+          fields: {
+            members: 1,
+            scenarioSetId: 1,
+            discussions: 1,
+            createdAt: 1,
+            // TODO: add fields to track scoring
+          },
+        },
+      );
   });
 }
